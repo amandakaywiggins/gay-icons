79d895e1b2a5d7d4cd042886d0ee034568780ae1
Amanda Kay <amandakaywiggins@gmail.com>
2018-05-11 20:40:41 -0500

    It's GAY now!

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,31 +1,30 @@
-var gayArray = ["Cher" , "Elton John" , "Lady Gaga" , "Hayley Kiyoko" , "Ru Paul" , "Ariana Grande" , "Liberace" , "Celine Dion" ,
-                "Judy Garland" , "Ellen Degeneres" , "Georgia Michael" , "Madonna"];
+var gayArray = ["Cher" , "Elton John" , "Hayley Kiyoko" , "Ru Paul" , "Ariana Grande" , "Celine Dion" ,
+                "Judy Garland" , "Ellen Degeneres" , "George Michael" , "Madonna"];
 
 function displayGayGifs() {
-    $("button").on("click", function() {
-        var gay = $(this).attr("data-gay");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gay + "&api_key=dc6zaTOxFJmzC&limit=10";
-
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function(response) {
-            console.log(response);
-            var results = response.data;
-            
-            for (var i = 0; i < results.length; i++) {
-                var gayDiv = $("<div>");
-                var rating = results[i].rating;
-                var p = $("<p>").text("Rating: " + rating);
-                var gayImage = $("<img>");
-                gayImage.attr("src", results[i].images.fixed_height.url);
-                gayDiv.append(p);
-                gayDiv.append(gayImage);
-                $("#display-gifs").prepend(gayDiv);
-            }
+    var gay = $(this).attr("data-gay");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gay + "&api_key=dc6zaTOxFJmzC&limit=10";
+        
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+        var results = response.data;
+        $("button").on("click", function() {
+        for (var i = 0; i < results.length; i++) {
+            var gayDiv = $("<div>");
+            var rating = results[i].rating;
+            var p = $("<p>").text("Rating: " + rating);
+            var gayImage = $("<img>");
+            gayImage.attr("src", results[i].images.fixed_height.url);
+            gayDiv.append(p);
+            gayDiv.append(gayImage);
+            $("#display-gifs").prepend(gayDiv);
+        }
         });
     });
-}
+};
     
 function renderButtons() {
     $("#new-buttons").empty();
@@ -34,9 +33,9 @@ function renderButtons() {
         a.addClass("gay");
         a.attr("data-gay", gayArray[i]);
         a.text(gayArray[i]);
-        $("#buttons-view").append(a);
-    }
-}
+        $("#new-buttons").append(a);
+    };
+};
 
 $("#add-button").on("click", function(event) {
     event.preventDefault();
